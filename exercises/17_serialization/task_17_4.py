71b058a70dc948cc29552002a41074362b94a003
aleksejpojda <aleksej.pojda@gmail.com>
2022-01-17 03:46:28 +0000

    tasks 17

--- exercises/17_serialization/task_17_4.py
+++ exercises/17_serialization/task_17_4.py
@@ -61,40 +61,24 @@ def convert_datetime_to_str(datetime_obj):
 import re
 import csv
 
-
-def list_dublicate(input_list, number_columns):
-    '''
-    number_columns номер столбца с нулевого в котором искать повторения
-    '''
-    for line in input_list:
-
-
-
-
 def write_last_log_to_csv(source_log, output):
-    data = {}
-    regex = r"(?P<name>.+?),(?P<Email>.+?),(?P<change>\d+.+)"
     with open(source_log) as f:
-#        output = f.read()
-        reader = csv.DictReader(f)
-        for row in reader:
-            data.update(row)
-        print()
-        headers = next(reader)
-        headers1 = re.match(r"(?P<Headers>\D+,\D+,\D+ \D+)\n", output.strip("\n"))
-        print(headers1.group("Headers"))
-        result_list = re.findall(regex, output.strip("\n"))
-        for line in result_list:
-            data.append(list(match.groups()))
-        print(data)
-
-#        print('Headers: ', headers)
-#        for row in reader:
-#            print(row)
-#            result_dict.update(row[1])
-#        print(result_dict)
- #       convert_str_to_datetime(line[2])
-
+        data = list(csv.reader(f))
+        header = data[0]
+    result = {}
+    sorted_by_date = sorted(
+        data[1:], key=lambda x: convert_str_to_datetime(x[2])
+            )
+    print(sorted_by_date)
+    for name, email, date in sorted_by_date:
+        result[email] = (name, email, date)
+    print("result")
+    print(result)
+    with open(output, "w") as dest:
+        writer = csv.writer(dest)
+        writer.writerow(header)
+        for row in result.values():
+            writer.writerow(row)
 
 if __name__ == "__main__":
-    print(write_last_log_to_csv("mail_log.csv", "out.txt"))
+    write_last_log_to_csv("mail_log.csv", "out.txt")
