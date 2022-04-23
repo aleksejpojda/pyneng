09e91ae68029f20adb7c69207c1a54af54aa557f
aleksejpojda <aleksej.pojda@gmail.com>
2022-04-24 02:34:26 +0300

    Task 6.2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,38 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
+address = input("Введите IP-адрес: ")
+
+if address.count(".") == 3:
+    if len(address.split(".")) == 4:
+        if address.split(".")[0].isdigit() and address.split(".")[1].isdigit() and address.split(".")[2].isdigit() and address.split(".")[3].isdigit():
+            if int(address.split(".")[0]) >= 0 and int(address.split(".")[0]) <= 255:
+                if int(address.split(".")[1]) >= 0 and int(address.split(".")[1]) <= 255:
+                    if int(address.split(".")[2]) >= 0 and int(address.split(".")[2]) <= 255:
+                        if int(address.split(".")[3]) >= 0 and int(address.split(".")[3]) <= 255:
+                            if int(address.split(".")[0]) >= 1 and int(address.split(".")[0]) <= 223:
+                                print("unicast")
+                            elif int(address.split(".")[0]) >= 224 and int(address.split(".")[0]) <= 239:
+                                print("multicast")
+                            elif address == "255.255.255.255":
+                                print("local broadcast")
+                            elif address == "0.0.0.0":
+                                print("unassigned")
+                            else:
+                                print("unused")
+                        else:
+                            print("Неправильный IP-адрес")
+                    else:
+                        print("Неправильный IP-адрес")
+                else:
+                    print("Неправильный IP-адрес")
+            else:
+                print("Неправильный IP-адрес")
+        else:
+            print("Неправильный IP-адрес")
+    else:
+        print("Неправильный IP-адрес")
+else:
+    print("Неправильный IP-адрес")
+
+
